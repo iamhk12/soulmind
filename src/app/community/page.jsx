ed8d01d0cc781f278145c178014f4639e697015e
Himanshu Kothari <hkothari247@gmail.com>
2024-02-23 23:38:53 +0530

    Changes : FetchPost Function ->  Commented loader

--- src/app/community/page.jsx
+++ src/app/community/page.jsx
@@ -197,7 +197,7 @@ const Community = () => {
     const [allposts, setAllPosts] = useState([])
 
     const FetchPosts = async (rev = false) => {
-        setIsLoading(true);
+        // setIsLoading(true);
         try {
             const resFromBack = await fetch('https://server-innercalm.vercel.app/api/allPosts', {
                 method: "GET",
