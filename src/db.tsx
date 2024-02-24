f05f44260aa1494147824e10962179a8f946dfc4
iamhk12 <hkothari247@gmail.com>
2024-02-24 15:43:29 +0530

    Changes : DB modified

--- src/db.tsx
+++ src/db.tsx
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     const db = "mongodb+srv://innercalm:innercalm@innercalm.hyeb5s1.mongodb.net/innercalm"
     try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(db, {});
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
