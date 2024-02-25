354f0c25a1419dd7adad7eaf250032069e46e9b6
iamhk12 <hkothari247@gmail.com>
2024-02-25 15:17:44 +0530

    Changes : vercel, speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Wrapper from "../app/ToastContainer"
 // const inter = Inter({ subsets: ["latin"] }); className={inter.className}
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,6 +23,7 @@ export default function RootLayout({
         </Wrapper>
         <div style={{display : "none"}}>
           <Analytics mode={'production'}/>;
+          <SpeedInsights />
         </div>
       </body>
       
